2438651a44f45b220454ba5063172ca3bb754fd6
King Robert Kehinde <robert.k@techie.com>
2023-04-18 11:47:21 +0100

    React Components

---
+++ src/Components/ClassComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a React Class Component</h1>
+      </div>
+    )
+  }
+}
